7c5f4e53de2549caa7afd4df1f88c4a6da023e03
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-19 23:09:26 +0300

    Update server.pl

--- server.pl
+++ server.pl
@@ -4,10 +4,13 @@
 
 :- http_handler(root(hello), say_hello, []).
 
+% Стартирай сървъра на даден порт
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
 
+% Обработва заявката към /hello
 say_hello(_Request) :-
     reply_json(json{message:"Hello from Prolog"}).
 
+% Автоматично стартиране при пускане на файла
 :- initialization(server(8080)).
