f7b51266bb336c13587b6f953a21b776f0116335
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-19 22:52:30 +0300

    Update server.pl

--- server.pl
+++ server.pl
@@ -1,20 +1,8 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_json)).
 
-% Стартира HTTP сървър на порт 8000
-server(Port) :-
-    http_server(http_dispatch, [port(Port)]).
+:- http_handler('/prolog_query', prolog_query, []).
 
-% Маршрут /query приема POST заявки с JSON
-:- http_handler(root(query), handle_query, []).
-
-% Обработчик на заявка
-handle_query(Request) :-
-    http_read_json_dict(Request, DictIn),
-    GoalString = DictIn.get(goal),
-    % Превръщаме низ в Prolog терм
-    term_string(GoalTerm, GoalString),
-    % Оценяваме терма (върни true/false)
-    (call(GoalTerm) -> Result = true ; Result = false),
-    reply_json_dict(_{result: Result}).
+prolog_query(Request) :-
+    format('Content-type: text/plain~n~n'),
+    format('Hello from Prolog!~n').
