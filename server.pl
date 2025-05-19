c092197e5cc4411aebe12c1a47ccef75b6aa47a6
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-19 23:19:12 +0300

    Update server.pl

--- server.pl
+++ server.pl
@@ -1,26 +1,19 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
-:- use_module(library(system)).
 
 :- http_handler(root(hello), say_hello, []).
+
+server(Port) :-
+    format('Starting server on port ~w~n', [Port]),
+    http_server(http_dispatch, [port(Port)]).
 
 say_hello(_Request) :-
     reply_json(json{message:"Hello from Prolog"}).
 
-% Вземи порта от променлива на средата или използвай 8080 по подразбиране
-get_port(Port) :-
-    getenv('PORT', PortString) ->
-    atom_number(PortString, Port)
-    ;
-    Port = 8080.
+:- initialization(start_server).
 
-:- initialization(main).
-
-main :-
-    get_port(Port),
-    format('Starting server on port ~w~n', [Port]),
+start_server :-
+    getenv('PORT', PortAtom),      % взимаме порта от средата
+    atom_number(PortAtom, Port),   % превръщаме го в число
     server(Port).
-
-server(Port) :-
-    http_server(http_dispatch, [port(Port)]).
