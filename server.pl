d7e58c8ef472f6f71139aaedab543ac7149a1a7e
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-20 16:59:12 +0300

    Update server.pl

--- server.pl
+++ server.pl
@@ -1,22 +1,14 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
-:- use_module(library(http/json)).
 
-:- dynamic server_started/0.
-
-% HTTP handler
 :- http_handler(root(hello), say_hello, []).
 
-% Say hello endpoint
 say_hello(_Request) :-
     reply_json(json{message:"Hello from Prolog"}).
 
-% Server entry point
 server :-
-    \+ server_started,
-    assert(server_started),
-    getenv('PORT', PortAtom),
+    getenv('PORT', PortAtom),           % Render задава PORT като env променлива
     atom_number(PortAtom, Port),
     format('Starting server on port ~w~n', [Port]),
     http_server(http_dispatch, [port(Port)]).
