afb012ebd9895acce3bed81fb3e8d7e1cc9105a9
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-20 17:00:37 +0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import requests
-
-app = FastAPI()
-
-@app.get("/query")
-def query_prolog():
-    response = requests.get("http://localhost:8000/prolog_query")
-    return {"prolog_response": response.text}
