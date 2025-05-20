c7895c7ca0b0813543fc4ea849d4b7406cf02245
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-20 21:44:55 +0300

    Update server.pl

--- server.pl
+++ server.pl
@@ -1,21 +1,21 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
+:- use_module(library(http/json)).
 
 :- http_handler(root(hello), say_hello, []).
 
 server(Port) :-
-    http_server(http_dispatch, [port(Port), ip('0.0.0.0')]).
+    http_server(http_dispatch, [port(Port)]).
 
 say_hello(_Request) :-
-    reply_json_dict(_{message:"Hello from Prolog REST API"}).
+    reply_json(json{message: "Hello from Prolog API"}).
 
-:- initialization(main, main).
-
-main :-
-    ( getenv('PORT', PortAtom) ->
-        atom_number(PortAtom, Port)
-    ; Port = 8080
+start_server :-
+    (   getenv('PORT', PortAtom)
+    ->  atom_number(PortAtom, Port)
+    ;   Port = 8080
     ),
-    format('Starting server on port ~w~n', [Port]),
     server(Port).
+
+:- initialization(start_server, main).
