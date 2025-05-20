edb214944db762260cb98e1ff8ba4dd8af2de0bf
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2025-05-20 20:46:12 +0300

    Update server.pl

--- server.pl
+++ server.pl
@@ -4,15 +4,16 @@
 
 :- http_handler(root(hello), say_hello, []).
 
-server(Port) :-
-    http_server(http_dispatch, [port(Port)]).
-
 say_hello(_Request) :-
     reply_json(json{message: "Hello from Prolog API"}).
 
-start_server :-
-    getenv('PORT', PortAtom),
+server(Port) :-
+    format("Starting server on port ~w~n", [Port]),
+    http_server(http_dispatch, [port(Port)]).
+
+:- initialization(main).
+
+main :-
+    getenv('PORT', PortAtom),         % <- взима порт от Railway
     atom_number(PortAtom, Port),
     server(Port).
-
-:- initialization(start_server).
